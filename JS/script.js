4f3a2e33f1f87c736335341a5f90f360292e1aa5
subhani <mrthug1710@gmail.com>
2025-10-08 08:18:36 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+let a = 18;
+if (a >= 18){
+    console.log("Adult!");
+}
+else{
+    console.log("Minor!");
+}
+
+for (let i = 1; i <10; i++){
+    console.log("iteration Number: "+ i);
+}
+
+function greet(name){
+    console.log("Hello " + name);
+}
+
+greet("sohan");
+greet("Rakhi");
+
+                                                                                                                                                                       
